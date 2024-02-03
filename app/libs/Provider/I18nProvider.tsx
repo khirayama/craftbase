406828e2b65368bf695765c04d2ec5cfd30881fe
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-02-03 12:06:44 +0900

    Create I18nProvider

---
+++ app/libs/Provider/I18nProvider.tsx
@@ -0,0 +1,78 @@
+import {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from "react";
+import i18next from "i18next";
+
+export type Lng = "ja" | "en";
+
+const jaTranslation = {
+  routes: {
+    index: {
+      'helloFromIndexPage': "トップページからこんにちわ"
+    }
+  }
+}
+
+const enTranslation = {
+  routes: {
+    index: {
+      'helloFromIndexPage': "Hello from Index Page"
+    }
+  }
+}
+
+const options = {
+  // detection: {
+  //   lookupQuerystring: "hl",
+  // },
+  fallbackLng: "ja" as Lng,
+  resources: {
+    en: {
+      translation: enTranslation,
+    },
+    ja: {
+      translation: jaTranslation,
+    },
+  },
+};
+
+export const init = () => {
+  i18next.init(options);
+}
+
+export const resolveLng = () => i18next.resolvedLanguage as Lng;
+
+export const supportedTranslations = () => Object.keys(options.resources);
+
+export const t = i18next.t;
+
+const I18nContext = createContext<
+  [Lng, Dispatch<SetStateAction<Lng>>]
+>([options.fallbackLng, () => {}]);
+
+export function useI18n(): [
+  Lng,
+  Function,
+] {
+  const [lng, nativeSetLang] = useContext(I18nContext);
+  const setLng = (lg: Lng) => {
+    i18next.changeLanguage(lg);
+    nativeSetLang(lg);
+  }
+  return [lng, setLng];
+}
+
+export function I18nProvider({ children }) {
+  const [, setLng] = useState<Lng>(options.fallbackLng);
+  const lng = resolveLng();
+
+  return (
+    <I18nContext.Provider value={[lng, setLng]}>
+     {children}
+    </I18nContext.Provider>
+  );
+}
