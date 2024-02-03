669b43b397f775a1ea30d60f90db24ca52a0362e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-02-03 12:07:11 +0900

    Create index file for Provider lib

---
+++ app/libs/Provider/index.tsx
@@ -0,0 +1,3 @@
+export {type ThemeOption, resolveTheme, themeOptions, useTheme, ThemeProvider} from './ThemeProvider';
+export {type Lng, resolveLng, init, t, supportedTranslations, useI18n, I18nProvider} from './I18nProvider';
+export {Provider} from './Provider';
